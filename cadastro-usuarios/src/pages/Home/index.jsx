cb3daa4ddad9b70ab49b86ac6ac6dc65b7c52f87
Daniel <114535778+danielbmata@users.noreply.github.com>
2024-11-29 10:29:21 -0300

    criar esqueleto do formulario

---
+++ cadastro-usuarios/src/pages/Home/index.jsx
@@ -0,0 +1,21 @@
+import './style.css'
+
+function Home() {
+  
+
+  return (
+    
+      <div className='container'>
+        <form>
+          <h1>Cadastro de Usuários</h1>
+          <input name='nome' type='text' />
+          <input name='idade' type='number' />
+          <input name='email' type='email' />
+          <button type='button'>Cadastrar</button>
+        </form>
+      </div>
+      
+  )
+}
+
+export default Home
