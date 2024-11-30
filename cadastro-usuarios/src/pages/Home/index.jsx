9888393dd4797cdeb60641cbf31b0ca1cb00a2ce
Daniel <114535778+danielbmata@users.noreply.github.com>
2024-11-30 11:08:33 -0300

    Adicionar useState

--- cadastro-usuarios/src/pages/Home/index.jsx
+++ cadastro-usuarios/src/pages/Home/index.jsx
@@ -1,49 +1,51 @@
-import { useEffect } from 'react'
-import './style.css'
-import Lixo from '../../assets/lixo.svg'
-import api from '../../services/api'
+import { useEffect, useState } from 'react';
+import './style.css';
+import Lixo from '../../assets/lixo.svg';
+import api from '../../services/api';
 
 function Home() {
+  const [users, setUsers] = useState([]);
 
-  let users = []
+  async function getUsers() {
+    const usersFromApi = await api.get('/usuarios');
 
-  async function getUsers(){
-    users = await api.get('/usuarios');
-  };
+    setUsers(usersFromApi.data);
+  }
 
   useEffect(() => {
-    getUsers()
-  }, [])
-  
-
+    getUsers();
+  }, []);
 
   return (
-
-    <div className='container'>
+    <div className="container">
       <form>
         <h1>Cadastro de Usuários</h1>
-        <input placeholder='Nome' name='nome' type='text' />
-        <input placeholder='Idade' name='idade' type='number' />
-        <input placeholder='Email' name='email' type='email' />
-        <button type='button'>Cadastrar</button>
+        <input placeholder="Nome" name="nome" type="text" />
+        <input placeholder="Idade" name="idade" type="number" />
+        <input placeholder="Email" name="email" type="email" />
+        <button type="button">Cadastrar</button>
       </form>
 
-      {users.map(user => (
-        <div key={user.id} className='card'>
+      {users.map((user) => (
+        <div key={user.id} className="card">
           <div>
-            <p>Nome: <span>{user.name}</span> </p>
-            <p>Idade: <span>{user.age}</span> </p>
-            <p>Email: <span>{user.email}</span> </p>
+            <p>
+              Nome: <span>{user.name}</span>{' '}
+            </p>
+            <p>
+              Idade: <span>{user.age}</span>{' '}
+            </p>
+            <p>
+              Email: <span>{user.email}</span>{' '}
+            </p>
             <button>
               <img src={Lixo} alt="" />
             </button>
           </div>
         </div>
       ))}
-
     </div>
-
-  )
+  );
 }
 
-export default Home
+export default Home;
--- cadastro-usuarios/src/services/api.js
+++ cadastro-usuarios/src/services/api.js
@@ -1,8 +1,8 @@
-// Conexão com o 
-import axios from "axios";
+// Conexão com o
+import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:3000/'
-})
+  baseURL: 'http://localhost:3000/',
+});
 
-export default api
+export default api;
